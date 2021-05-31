4a569a33076e11d0a3181da6c5de80588c3b17b8
AndreAguirreRoman <ankar_aguirre@live.com>
2021-05-31 14:27:58 -0500

    finishe signup form and commit

--- src/components/auth/signupForm.js
+++ src/components/auth/signupForm.js
@@ -1,33 +1,29 @@
-
 import React, { Component } from 'react';
 
 import { reduxForm, Field } from 'redux-form';
 
 import { FormInput, FormButton } from '../formFields';
 import Details from '../details';
+
 import history from '../../history';
 
 class SignUpForm extends Component {
     render() {
         const { className, handleSubmit } = this.props;
-        const links = [
+        const info = [
             {
                 _id: 0,
-                title: 'Not registered? Create account here',
-                onClick: () => history.push('/signup')
+                title: 'At least 6 characters'
             },
             {
                 _id: 1,
-                title: 'Forgot account email?',
-                onClick: () => console.log('forgot email')
+                title: 'At least one number'
             },
             {
                 _id: 2,
-                title: 'Forgot password?',
-                onClick: () => console.log('forgot password')
+                title: 'At least one symbol'
             }
         ]
-
         return (
             <form onSubmit={handleSubmit} className={`${className} sign-up-form`}>
                 <Field className='sign-up-form__name'
@@ -35,59 +31,48 @@ class SignUpForm extends Component {
                     title='Name'
                     placeholder='Name'
                     name='name'
-                    component={FormInput}
-                />
+                    component={FormInput} />
                 <Field className='sign-up-form__email'
                     type='email'
                     title='Email'
                     placeholder='Email'
                     name='email'
-                    component={FormInput}
-                />
-
-
+                    component={FormInput} />
                 <Field className='sign-up-form__password'
                     type='password'
                     title='Password'
                     placeholder='Password'
                     name='password'
-                    component={FormInput}
-                />
-                <Field className='sign-up-form__confirm-password'
+                    component={FormInput} />
+                <Field className='sign-up-form__confirm'
                     type='password'
                     title='Confirm Password'
                     placeholder='Confirm Password'
                     name='confirm'
-                    component={FormInput}
-                />
-
+                    component={FormInput} />
 
                 <div className='sign-up-form__line'></div>
                 <Field className='sign-up-form__login'
-                    onClick={() => console.log('tryna submit')}
+                    onClick={() => history.push('/account')}
                     type='submit'
-                    title='Login'
+                    title='Create Account'
                     name='login'
-                    component={FormButton}
-                />
+                    component={FormButton} />
                 <Field className='sign-up-form__back'
-                    onClick={() => console.log('tryna go back')}
+                    onClick={() => history.push('/signin')}
                     type='button'
                     title='Back'
                     name='back'
                     short={true}
-                    component={FormButton}
-                />
-                <Details className='sign-up-form__details' title='QuickLinks' links={links} />
-            </ form>
-        );
+                    component={FormButton} />
+                <Details className='sign-up-form__details' title='Password Requirements' info={info} />
+            </form>
+        )
     }
 }
 
 SignUpForm = reduxForm({
     form: 'SignUpForm'
 })(SignUpForm);
-
-
 
 export default SignUpForm;
