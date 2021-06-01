91f3f5af8af1b2995a90c719762a4bd5e5f801c7
AndreAguirreRoman <ankar_aguirre@live.com>
2021-06-01 18:31:47 -0500

    setup purchase history child component

---
+++ src/components/account/purchases.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+
+
+class Purchases extends Component {
+
+    render() {
+
+        const { className } = this.props;
+
+        return (
+            <div className={`${className} purchases`}>
+                Purchases goes here...
+            </div>
+        )
+
+    }
+}
+
+export default Purchases;
