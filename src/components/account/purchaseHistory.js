a47aab655ad0aa830b62050da6b60c700e4fed77
AndreAguirreRoman <ankar_aguirre@live.com>
2021-06-01 15:02:59 -0500

    developed navbar and header actions for setting new links and used in account.js

---
+++ src/components/account/purchaseHistory.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class PurchaseHistory extends Component {
+    render() {
+        return (
+            <div>
+                PurchaseHistory
+            </div>
+        )
+
+    }
+}
+
+export default PurchaseHistory;
