47e2200b2658c9029e56d10df1113c912b270943
AndreAguirreRoman <ankar_aguirre@live.com>
2021-06-01 15:25:10 -0500

    finished header feature

--- src/components/auth/signin.js
+++ src/components/auth/signin.js
@@ -2,10 +2,17 @@ import React, { Component } from 'react';
 
 import SignInForm from './signinForm';
 import PageTitle from '../pageTitle';
+import { connect } from 'react-redux';
+import * as actions from '../../actions';
 
 
 
 class SignIn extends Component {
+
+    componentDidMount() {
+        this.props.setHeaderLinks([]);
+        this.props.setNavbarLinks([]);
+    }
 
     onSubmit = (fields) => {
         console.log(fields)
@@ -22,5 +29,5 @@ class SignIn extends Component {
     }
 }
 
-
+SignIn = connect(null, actions)(SignIn);
 export default SignIn;
