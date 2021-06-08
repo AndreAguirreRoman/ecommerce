6e5c6286f60249fc5d2158307ffe1dccb7d1b82b
AndreAguirreRoman <ankar_aguirre@live.com>
2021-06-08 11:33:21 -0500

    Created the shop compionent and the basic skeleton for the components, added the navbar links as test

---
+++ src/components/shop/shop.js
@@ -0,0 +1,46 @@
+import React, { Component } from 'react';
+
+import { connect } from 'react-redux';
+import * as actions from '../../actions';
+
+class Shop extends Component {
+
+    componentDidMount() {
+        const headerLinks = [
+            {
+                _id: 0,
+                title: 'Shop',
+                path: '/shop'
+            },
+            {
+                _id: 1,
+                title: 'Logout',
+                path: '/'
+            }
+        ]
+        this.props.setHeaderLinks(headerLinks)
+        // set header links
+        //fetch navbar links
+        // set navbar links
+        //filter products 
+    }
+
+
+    render() {
+        return (
+            <div className='shop'>
+                {/* SHOP SEARCH BAR */}
+                {/* SHOP BUTTON */}
+                {/* SHOP CART */}
+            </div>)
+    }
+
+}
+
+function mapStateToProps(state) {
+    return { state }
+}
+
+Shop = connect(mapStateToProps, actions)(Shop)
+
+export default Shop;
