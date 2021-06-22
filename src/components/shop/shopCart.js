7ebe42892d948333af0992a1fb1dbed8bfb6837d
AndreAguirreRoman <ankar_aguirre@live.com>
2021-06-22 13:12:07 -0500

    styled the shop cart, title, products and the footer specially to take all the bottom space (grids)

--- src/components/shop/shopCart.js
+++ src/components/shop/shopCart.js
@@ -51,7 +51,7 @@ class ShopCart extends Component {
         return (
             <div className={`${className} shop-cart`}>
                 <CartButton className='shop-cart__toggle' icon='fas fa-times' />
-                <CartContent className='shop-cart__content' products={[4, 234, 222]} />
+                <CartContent className='shop-cart__content' products={[4, 234, 222, 4, 234, 222, 4, 234, 222, 4, 234, 222, 4, 234, 222, 4, 234, 222, 123]} />
             </div>
         )
     }
